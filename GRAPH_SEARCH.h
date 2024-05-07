59755f37ea9c25ff3fa06916cdf3cab2c7d1a61b
baha4 <baha4@DESKTOP-PB166BB>
2024-05-07 22:33:51 +0300

    x

--- GRAPH_SEARCH.h
+++ GRAPH_SEARCH.h
@@ -8,7 +8,7 @@
 
 // ========  YOUR COMPULSORY (BUT SPECIFIC TO THE PROBLEM) COMMANDS ============ 
 #define PREDETERMINED_GOAL_STATE  1 // User will initially determine the goal state if it is true (1)
-#define ACTIONS_NUMBER  20          // The number of all possible actions
+#define ACTIONS_NUMBER  64          // The number of all possible actions
 #define MAX_SEARCHED_NODE 100000000 // exit from the search process if it is exceeded  
 
 // ================== YOU DO NOT NEED TO CHANGE THIS PART ======================
